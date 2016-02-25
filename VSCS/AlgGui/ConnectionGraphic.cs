bd4cd1674e48d8ef5a3e5c48f546e5bab20f18be
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-02-25 15:01:40 -0600

    Started working on ConnectionGraphic

--- VSCS/AlgGui/ConnectionGraphic.cs
+++ VSCS/AlgGui/ConnectionGraphic.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace AlgGui
 {
 	class ConnectionGraphic
 	{
+		// member variables
+		private Line m_body = new Line();
+		private Label m_typename = new Label();
+		
+		//private Node m_origin
+
+		// construction
+		public ConnectionGraphic()
+		{
+
+		}
 	}
 }
